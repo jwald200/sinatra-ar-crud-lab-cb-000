85d197f726facdd9541f72b13a1e92dcbba5e64c
Jacob Wald <schoen575@gmail.com>
2017-04-30 16:50:37 +0000

    Done.

--- Rakefile
+++ Rakefile
@@ -7,5 +7,9 @@ require './app/controllers/application_controller'
 # Type `rake -T` on your command line to see the available rake tasks.
 
 task :console do
+  def reload!
+    load_all 'app'
+  end
+  
   Pry.start
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,52 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  #create
+  get '/posts/new' do
+    erb :new
+  end
+
+  post '/posts' do
+    Post.create(params)
+
+    redirect "/posts"
+  end
+
+  #read
+  get '/posts' do
+    @posts = Post.all
+
+    erb :index
+  end
+
+  get '/posts/:id' do
+    @post = Post.find_by_id(params[:id])
+
+    erb :show
+  end
+
+  #update
+  get '/posts/:id/edit' do
+    @post = Post.find_by_id(params[:id])
+
+    erb :edit
+  end
+
+  patch '/posts/:id' do
+    post = Post.find_by_id(params[:id])
+    post.name = params[:name]
+    post.content = params[:content]
+    post.save
+
+    redirect "/posts/#{post.id}"
+  end
+
+  #delete
+
+  delete '/posts/:id/delete' do
+    @post = Post.find_by_id(params[:id])
+    @post.delete
+
+    erb :delete
   end
 end
